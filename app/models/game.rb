72a305060cf3d42e995801ce3c7deda1ed33e8b8
collinl34 <collinlshelby@gmail.com>
2016-12-07 17:38:08 -0800

    join table model create for game_tags table

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,9 @@
 class Game < ApplicationRecord
+  has_many   :game_tags
+  has_many   :tags, through: :game_tags
   belongs_to :creator, class_name: 'User'
   has_many   :user_games
   has_many   :users, through: :user_games
   has_many   :votes, as: :voteable
   has_many   :comments, as: :commentable
-  has_many   :tags
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
-  has_many :games
+  has_many :game_tags
+  has_many :games, through: :game_tags
 end
