38f69e08d81f35c533d744e319e7eebe07d4b387
collinl34 <collinlshelby@gmail.com>
2016-12-08 09:01:46 -0800

    styling added to migration code

--- db/migrate/20161208001816_create_games.rb
+++ db/migrate/20161208001816_create_games.rb
@@ -1,10 +1,10 @@
 class CreateGames < ActiveRecord::Migration[5.0]
   def change
     create_table :games do |t|
-      t.string  :name, :image, :thumbnail
-      t.text    :description
-      t.integer :min_players, :max_players, :year_published, :playing_time
-      t.integer :creator_id
+      t.string   :name, :image, :thumbnail
+      t.text     :description
+      t.integer  :min_players, :max_players, :year_published, :playing_time
+      t.integer  :creator_id
 
       t.timestamps
     end
