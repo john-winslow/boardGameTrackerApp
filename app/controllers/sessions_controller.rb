a32b2a20229382379336ded3108aafad8c6954a6
John Winslow <winslow.johnm@gmail.com>
2016-12-08 14:52:20 -0800

    create logout functionality (controllers, helpers)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid username/password combination'
+      render 'new'
+    end
+  end
+
+  def delete
+    log_out
     redirect_to '/'
   end
+
+  def destroy
+    log_out
+    redirect_to '/'
+  end
+
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
+  root 'users#index'
 
   get '/login'      => 'sessions#new'
   post '/login'     => 'sessions#create'
-  delete '/logout ' => 'sessions#destroy'
+  get 'logout'      => 'sessions#delete'
+  # delete '/logout ' => 'sessions#destroy'
 end
