6240282c823e5472c918c6f70696d536ff0c843b
collinl34 <collinlshelby@gmail.com>
2016-12-09 12:31:37 -0800

    pull from development

--- db/schema.rb
+++ db/schema.rb
@@ -25,13 +25,6 @@ ActiveRecord::Schema.define(version: 20161209052801) do
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id", using: :btree
   end
 
-  create_table "friends", force: :cascade do |t|
-    t.string   "username"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "game_tags", force: :cascade do |t|
     t.integer  "game_id"
     t.integer  "tag_id"
